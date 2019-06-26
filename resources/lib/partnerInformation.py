f1d2a38519f3a624f37db6f8dd013ecdfce164ee
Mark König <mark.koenig@kleiner-schelm.de>
2019-06-26 21:46:43 +0200

    Add files via upload

---
+++ resources/lib/partnerInformation.py
@@ -0,0 +1,18 @@
+import json
+
+class partnerInformation(object):
+
+    # nur ein object
+    name = ''
+    rentPrice = 0
+    buyPrice = 0
+
+    def __init__(self, data):
+        self.jData = data
+
+        if 'name' in self.jData: self.name = self.jData['name']
+        if 'rentPrice' in self.jData: self.rentPrice = self.jData['rentPrice']
+        if 'buyPrice' in self.jData: self.buyPrice = self.jData['buyPrice']
+
+
+
